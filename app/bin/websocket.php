012946d68de19d378dd19c421c2bb5d5ba2307d6
granger kenolane <kenolane28@gmail.com>
2022-10-04 14:46:29 +0200

    Change the name of the class Mychat to router for more understanding

--- app/bin/websocket.php
+++ app/bin/websocket.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Service\MyChat;
+use App\Service\Router;
 use Ratchet\App;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 $app = new App('localhost', 9001, '0.0.0.0');
-$app->route('/', new MyChat, array('*'));
+$app->route('/', new Router, array('*'));
 $app->run();
 
 ?>
